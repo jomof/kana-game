c5d9b0ec0582877a89ab73718954f63d87a76998
Jomo Fisher <jomofisher@gmail.com>
2025-05-10 15:34:35 +0000

    Fix tests

--- src/test/kana-game_test.ts
+++ src/test/kana-game_test.ts
@@ -17,24 +17,57 @@ suite('kana-game', () => {
 
   test('renders with default values', async () => {
     const el = await fixture(html`<kana-game></kana-game>`);
+    const shadowRoot = el.shadowRoot;
+    assert.ok(shadowRoot, 'Shadow root should be present');
+    const input = shadowRoot.querySelector('#kana-input');
+    assert.ok(input, 'input should be present');
+    input.removeAttribute('data-wanakana-id');
+    input.removeAttribute('data-previous-attributes');
+
     assert.shadowDom.equal(
       el,
       `
       <h1>Hello, World!</h1>
       <button part="button">Click Count: 0</button>
       <slot></slot>
+      <input
+       autocapitalize="none"
+       autocomplete="off"
+       autocorrect="off"
+       id="kana-input"
+       lang="ja"
+       placeholder="Type in romaji..."
+       spellcheck="false"
+       type="text"
+      >
     `
     );
   });
 
   test('renders with a set name', async () => {
     const el = await fixture(html`<kana-game name="Test"></kana-game>`);
+    const shadowRoot = el.shadowRoot;
+    assert.ok(shadowRoot, 'Shadow root should be present');
+    const input = shadowRoot.querySelector('#kana-input');
+    assert.ok(input, 'input should be present');
+    input.removeAttribute('data-wanakana-id');
+    input.removeAttribute('data-previous-attributes');
     assert.shadowDom.equal(
       el,
       `
       <h1>Hello, Test!</h1>
       <button part="button">Click Count: 0</button>
       <slot></slot>
+      <input
+       autocapitalize="none"
+       autocomplete="off"
+       autocorrect="off"
+       id="kana-input"
+       lang="ja"
+       placeholder="Type in romaji..."
+       spellcheck="false"
+       type="text"
+      >
     `
     );
   });
@@ -44,12 +77,28 @@ suite('kana-game', () => {
     const button = el.shadowRoot!.querySelector('button')!;
     button.click();
     await el.updateComplete;
+    const shadowRoot = el.shadowRoot;
+    assert.ok(shadowRoot, 'Shadow root should be present');
+    const input = shadowRoot.querySelector('#kana-input');
+    assert.ok(input, 'input should be present');
+    input.removeAttribute('data-wanakana-id');
+    input.removeAttribute('data-previous-attributes');
     assert.shadowDom.equal(
       el,
       `
       <h1>Hello, World!</h1>
       <button part="button">Click Count: 1</button>
       <slot></slot>
+      <input
+       autocapitalize="none"
+       autocomplete="off"
+       autocorrect="off"
+       id="kana-input"
+       lang="ja"
+       placeholder="Type in romaji..."
+       spellcheck="false"
+       type="text"
+      >
     `
     );
   });
