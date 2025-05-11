bcae5aeb8ce20e9288872aa581bae5abde8fa970
Jomo Fisher <jomofisher@gmail.com>
2025-05-11 16:39:53 +0000

    Update setup-dev

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,7 +1,10 @@
-sudo apt-get update
-sudo npm ci
-sudo npx playwright install-deps 
-sudo npx playwright install
+echo apt-get update----------------------------------------------------- &> setup-dev.log
+sudo apt-get update &>> setup-dev.log
+echo npm ci------------------------------------------------------------- &>> setup-dev.log
+sudo npm ci &>> setup-dev.log
+echo npx playwright install-deps---------------------------------------- &>> setup-dev.log
+sudo npx playwright install-deps &>> setup-dev.log
+echo apt-get install---------------------------------------------------- &>> setup-dev.log
 sudo apt-get install -y \
     libgtk-4-1 \
     libgraphene-1.0-0 \
@@ -15,4 +18,7 @@ sudo apt-get install -y \
     libsecret-1-0 \
     libhyphen0 \
     libmanette-0.2-0 \
-    libgles2
+    libgles2 &>> setup-dev.log
+echo npx playwright install--------------------------------------------- &>> setup-dev.log
+sudo npx playwright install &>> setup-dev.log
+
