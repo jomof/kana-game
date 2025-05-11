84af0057d08c1a766c0a4c339e5a4a9c8f4e7cdf
Jomo Fisher <jomofisher@gmail.com>
2025-05-11 16:20:44 +0000

    Remove unfound

--- setup-dev.sh
+++ setup-dev.sh
@@ -6,14 +6,11 @@ sudo apt-get install -y \
     libgtk-4-1 \
     libgraphene-1.0-0 \
     libwoff1 \
-    libvpx9 \
     libevent-2.1-7 \
     libopus0 \
     libgstreamer1.0-0 \
     libgstreamer-plugins-base1.0-0 \
-    libgstreamer-plugins-good1.0-0 \
     libflite1 \
-    libavif16 \
     libharfbuzz-icu0 \
     libsecret-1-0 \
     libhyphen0 \
